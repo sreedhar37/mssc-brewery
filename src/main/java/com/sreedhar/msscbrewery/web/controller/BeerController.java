924a1a015b17315c8ec6c27a2c67021fdd08b677
Sreedhar Sudha <shreedhar.s@gmail.com>
2020-08-15 00:45:01 +0800

    brewery service with first api and couple of tests

---
+++ src/main/java/com/sreedhar/msscbrewery/web/controller/BeerController.java
@@ -0,0 +1,40 @@
+package com.sreedhar.msscbrewery.web.controller;
+
+import com.sreedhar.msscbrewery.services.BeerService;
+import com.sreedhar.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+    private final BeerService beerService;
+
+    public BeerController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
+        return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> handlePost(@RequestBody BeerDto beerDto) {
+        BeerDto saveDto = beerService.saveNewBeer(beerDto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("location", "/api/v1/beer" + saveDto.getId().toString());
+        return new ResponseEntity<>(headers,  HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity <String> handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto) {
+        beerService.updateBeer(beerId, beerDto);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
+
